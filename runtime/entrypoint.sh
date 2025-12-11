09685f845218c467633e2cdb2fc2812b4bd0fd92
Mateus Morais Dutra <mmdutra14@gmail.com>
2025-12-10 22:43:56 -0300

    fix zipfile path

--- runtime/entrypoint.sh
+++ runtime/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-base64 -d /app/functionzipfileb64 > /app/functionzipfile.zip
+base64 -d /app/functionzipfile64 > /app/functionzipfile.zip
 unzip /app/functionzipfile.zip -d /app
 python -m /app/main.py
